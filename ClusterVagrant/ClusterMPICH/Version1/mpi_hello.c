a4b3115deb68ad59c6a7727b502d6a6bbf2a30a4
Hector Fabio Jimenez S <hfjimenez@utp.edu.co>
2020-03-10 18:00:33 -0500

    Order Version1

--- ClusterVagrant/ClusterMPICH/Version1/mpi_hello.c
+++ ClusterVagrant/ClusterMPICH/Version1/mpi_hello.c
@@ -4,12 +4,11 @@
 int main(int argc, char** argv) {
     int myrank, nprocs;
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-
+    MPI_Init(&argc, &argv);     // Null Values, This will create the basic allocation for vars
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs); //Get the Communicator Size
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank); //For each instance we setup the Rank id
     printf("Hello from processor %d of %d\n", myrank, nprocs);
 
-    MPI_Finalize();
+    MPI_Finalize();     //garbage collection of all MPI vars. 
     return 0;
 }
