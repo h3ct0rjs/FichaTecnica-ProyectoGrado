482cf1678e21ad61d82b1cd1239a10a99fb8397b
Hector F. Jimenez S <h3ct0rjs@gmail.com>
2020-09-16 00:11:46 -0500

    Stable and Working Version. Add processor name to mpi hello world

--- ClusterVagrant/ClusterMPICH/Version1/mpi_hello.c
+++ ClusterVagrant/ClusterMPICH/Version1/mpi_hello.c
@@ -7,8 +7,14 @@ int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);     // Null Values, This will create the basic allocation for vars
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs); //Get the Communicator Size
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank); //For each instance we setup the Rank id
-    printf("Hello from processor %d of %d\n", myrank, nprocs);
+    
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
 
+    printf("Hello world from processor %s, rank %d out of %d processors\n",
+         processor_name, world_rank, world_size);
     MPI_Finalize();     //garbage collection of all MPI vars. 
     return 0;
 }
